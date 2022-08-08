db056afe0a5f39ac2605eed24e5d31779784c1ee
shubham8830h <shubhamhanamane1111@gmail.com>
2022-08-08 14:43:30 +0530

    change

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const lodash = require("lodash");
 const abc = require("../introduction/intro");
 const xyz = require("../logger/logger");
 const mno = require("../util/helper");
@@ -16,6 +17,46 @@ router.get("/test-assgnment", function (req, res) {
   xyz.welcome();
   mno.dateprint();
   def.allfunction();
+
+  let arr = [
+    "januray",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
+  let result = lodash.chunk(arr, 3);
+  console.log(result);
+
+  const arr2 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+  let result2 = lodash.tail(arr2);
+  console.log(result2);
+
+  const arr3 = [1, 2, 3];
+  const arr4 = [2, 3, 4];
+  const arr5 = [3, 2, 1];
+  const arr6 = [2, 1, 3];
+  const arr7 = [2, 3, 1];
+  const result3 = lodash.union(arr3, arr4, arr5, arr6, arr7);
+  console.log(result3);
+
+  const arr8 = [
+    ["horror", "The Shining"],
+    ["drama", "Titanic"],
+    ["thriller", "Shutter Island"],
+    ["fantasy", "Pans Labyrinth"],
+  ];
+
+  result4 = lodash.fromPairs(arr8);
+  console.log(result4);
+
   res.send("This is the second routes implementation");
 });
 
