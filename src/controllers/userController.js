3b9a836424bfc8df1b6759b4999bb5ce9481c97d
shubham8830h <shubhamhanamane1111@gmail.com>
2022-09-01 15:23:05 +0530

    this is auth 2 assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,7 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-/*
-  Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
-*/
+
 
 //1
 const createUser = async function (req, res) {
@@ -25,12 +23,7 @@ const loginUser = async function (req, res) {
       msg: "username or the password is not corerct",
     });
 
-  // Once the login is successful, create the jwt token with sign function
-  // Sign function has 2 inputs:
-  // Input 1 is the payload or the object containing data to be set in token
-  // The decision about what data to put in token depends on the business requirement
-  // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
-  // The same secret will be used to decode tokens 
+  
   let token = jwt.sign(
     {
       userId: user._id.toString(),
@@ -46,21 +39,14 @@ const loginUser = async function (req, res) {
 
 //3
 const getUserData = async function (req, res) {
-  // let token = req.headers["x-Auth-token"];
-  // if (!token) token = req.headers["x-auth-token"];
-
-  // //If no token is present in the request header return error. This means the user is not logged in.
-  // if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  // console.log(token);
-
+  
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
 
   res.send({ status: true, data: userDetails });
-  // Note: Try to see what happens if we change the secret while decoding the token
+  
 };
 
 //4
